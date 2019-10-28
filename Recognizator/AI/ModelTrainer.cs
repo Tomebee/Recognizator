740bb212fca317ee7d12dd1404e2469fa38aedf8
Tomasz Belczyk <tomasz.belczyk@esatto.se>
2019-10-28 14:37:20 +0100

    Removed comments

--- Recognizator/AI/ModelTrainer.cs
+++ Recognizator/AI/ModelTrainer.cs
@@ -41,28 +41,3 @@ namespace Recognizator.AI
         }
     }
 }
-
-//var imageData = context.Data.LoadFromEnumerable(images);
-//var imageDataShuffled = context.Data.ShuffleRows(imageData);
-//var a = context.Transforms.Conversion
-//    .MapValueToKey(nameof(Image.Label),
-//        nameof(Image.Label),
-//        keyOrdinality: ValueToKeyMappingEstimator.KeyOrdinality.ByValue)
-//    .Append(context.Transforms.LoadImages(
-//        "Image",
-//        _imagesPath,
-//        nameof(Image.Path)))
-//    .Append(context.Transforms.ResizeImages("Image", imageWidth: 224, imageHeight: 224))
-//    .Append(context.Transforms.ExtractPixels("Pixels", "Image"))
-//    .Append(context.Transforms.DnnFeaturizeImage("ImageData", dnnInput =>
-//    {
-//        return dnnInput.ModelSelector.ResNet18(context, dnnInput.OutputColumn, dnnInput.InputColumn);
-//    }, "Pixels"))
-//    .Append(context.Model.ImageClassification(
-//        "ImageData",
-//        nameof(Image.Label),
-//        arch: DnnEstimator.Architecture.ResnetV2101,
-//        epoch: 100
-//        ));
-//var testTrainData = context.Data.TrainTestSplit(imageDataShuffled, testFraction: 0.2);
-//var conversion = context.Transforms.Conversion.MapValueToKey(nameof(Image.Label), nameof(Image.Label), keyOrdinality: ValueToKeyMappingEstimator.KeyOrdinality.ByValue);
